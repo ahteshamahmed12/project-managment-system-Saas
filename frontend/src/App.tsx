f906c94d0b7099cff372caa231e460f1147d00e2
zainqaimi <138384994+zainqaimi@users.noreply.github.com>
2026-08-01 21:14:40 +0500

    frontend setup
    1.	React + Vite + TypeScript project setup.
    2.	Tailwind CSS integration and configuration.
    3.	React Router setup and initial folder structure creation.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-center py-80 text-orange-500 underline">
+        Project Setup Done
+      </h1>
+    </div>
+  );
+}
